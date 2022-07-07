3b6c9bf5e9b1d15437cffc77f6f2b10f1e53b4df
yaaroub <yarobdaoud1997@gmail.com>
2022-07-07 15:12:46 +0200

    22222222

---
+++ solution.js
@@ -0,0 +1,26 @@
+//1
+const isDog = true ? "pat, pat" : "find me a dog to pat!";
+console.log(isDog);
+//2----------------------
+const speedLimit = 50;
+const yourSpeed = 20;
+const ergebnis =
+  yourSpeed > speedLimit
+    ? "you're going too fast!"
+    : "You're driving below the speed limit, Oma";
+console.log(ergebnis);
+//3------------------------
+const age = 19;
+const ergebnis2 = age <= 16 ? "serve butter bee" : "serve beer";
+console.log(ergebnis2);
+//4------------------------
+const isStudent = true ? "Ticket costs €5,00" : "Ticket costs €12,00";
+console.log(isStudent);
+//5------------------------
+const okMarie = "cake";
+const ergebnis3 = okMarie == "cake" ? "Let them eat cake" : "Oh, bother";
+console.log(ergebnis3)
+//6
+//const zahl1 = 30
+const antwort30 = zahl1 % 2 === 0 ?`${zahl1} ist `
+//----------------------------------------------------------------------------------
